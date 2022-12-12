0fcce035b804876e20e85462067c2651f340202c
Tushar Singh <tusharrajpoot1972@gmail.com>
2022-12-12 18:29:55 +0530

    initial 1.1 commit

--- Git_1/multi.java
+++ Git_1/multi.java
@@ -1,7 +1,7 @@
 class multi
 {
     public  static void main(String rsg[]) {
-        int a=7,b=8;
+        int a=7,b=10;
         int c=a*b;
         System.out.println("product of two numbers is :"+c);
         
