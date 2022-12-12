0d4a467312502e6e981ce3a42712710a09b18d9c
Tushar Singh <tusharrajpoot1972@gmail.com>
2022-12-12 18:27:03 +0530

    difference commit

---
+++ Git_1/diff.java
@@ -0,0 +1,10 @@
+public class diff {
+      public  static void main(String rsg[]) {
+            int a=7,b=8;
+            int c=a-b;
+            System.out.println("diff of two numbers is :"+c);
+            
+        }
+    }
+    
+
